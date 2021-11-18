3ccd78a198f3ce1dfd1a85ef8464e4bd88e614ae
BebanSaya <94538727+BebanSaya@users.noreply.github.com>
2021-11-18 16:02:09 +0700

    Update and rename plugins/menu.js to plugin/menu.js

--- plugin/menu.js
+++ plugin/menu.js
@@ -47,9 +47,8 @@ const defaultMenu = {
 │ Uptime: *%uptime (%muptime)*
 │ Database: %rtotalreg of %totalreg
 │ Github :
-│ https://github.com/LitRHap/wabot
-│ Instagram :
-│ https://instagram.com/loli._.school
+│ Owner: https://wa.me/6281234816037
+│ Owner: https://wa.me/6289526350998
 ╰────
 %readmore`.trimStart(),
   header: '┏┉┄┈┈┈『%category』┈┈┈┈┈┉┓',
